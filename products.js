4f85716345cec51c9c30359a1149a9c8647ee4e6
Mekdes <yesmek@yahoo.com>
2020-03-30 17:22:32 +0200

    Added products list script

---
+++ products.js
@@ -0,0 +1,165 @@
+const products = [
+  {
+    id: "101",
+    type: "jens",
+    name: "clothes",
+    img: "https://source.unsplash.com/200x200?trousers",
+    price: "150",
+    color: "black",
+    description: "Jeans Straight Leg",
+    rating: 5
+  },
+
+  {
+    id: "102",
+    type: "jens",
+    name: "clothes",
+    img: "https://source.unsplash.com/200x200?cloth",
+    price: "130",
+    color: "blue",
+    description: "Jeans Relaxed Fit",
+    rating: 5
+  },
+
+  {
+    id: "103",
+    type: "top",
+    name: "clothes",
+    img: "https://source.unsplash.com/200x200?shirt",
+    price: "100",
+    color: "white",
+    description: "SPAGHETTITRÄGER-TOP",
+    rating: 4
+  },
+
+  {
+    id: "104",
+    type: "top",
+    name: "clothes",
+    img: "https://source.unsplash.com/200x200?sweaters",
+    price: "80",
+    color: "blue",
+    description: "JUNO - Knitwear",
+    rating: 5
+  },
+
+  {
+    id: "105",
+    type: "dress",
+    name: "clothes",
+    img: "https://source.unsplash.com/200x200?dress",
+    price: "150",
+    color: "black",
+    description: "Pencil skirt",
+    rating: 5
+  },
+
+  {
+    id: "106",
+    type: "dress",
+    name: "clothes",
+    img: "https://source.unsplash.com/200x200?skirt",
+    price: "100",
+    color: "black",
+    description: "cowboy skirts",
+    rating: 5
+  },
+
+  {
+    id: "107",
+    type: "flatshoe",
+    name: "shoe",
+    img: "https://source.unsplash.com/200x200?high hills",
+    price: "300",
+    color: "black",
+    description: "Sandals",
+    rating: 5
+  },
+
+  {
+    id: "108",
+    type: "flatshoe",
+    name: "shoe",
+    img: "https://source.unsplash.com/200x200?shoe",
+    price: "300",
+    color: "white",
+    description: "Moccasins",
+    rating: 5
+  },
+
+  {
+    id: "109",
+    type: "sneakers",
+    name: "shoe",
+    img: "https://source.unsplash.com/200x200?sneaker",
+    price: "400",
+    color: "pink",
+    description: "Sneakers",
+    rating: 3
+  },
+
+  {
+    id: "1010",
+    type: "sneakers",
+    name: "shoe",
+    img: "https://source.unsplash.com/200x200?sneaker",
+    price: "450",
+    color: "black",
+    description: "Sneakers high",
+    rating: 4
+  },
+
+  {
+    id: "1011",
+    type: "boots",
+    name: "shoe",
+    img: "https://source.unsplash.com/200x200?boots",
+    price: "550",
+    color: "black",
+    description: "Ankle boots",
+    rating: 4
+  },
+
+  {
+    id: "1012",
+    type: "boots",
+    name: "shoe",
+    img: "https://source.unsplash.com/200x200?boots",
+    price: "550",
+    color: "black",
+    description: "boots",
+    rating: 5
+  },
+
+  {
+    id: "1013",
+    type: "mekeup",
+    name: "beauty",
+    img: "https://source.unsplash.com/200x200?mekeup",
+    price: "250",
+    description: "100ML - Primer",
+    rating: 5
+  },
+
+  {
+    id: "1014",
+    type: "mekeup",
+    name: "beauty",
+    img: "https://source.unsplash.com/200x200?lipstick",
+    price: "130",
+    description: "JELLY SHOT- Lip balm",
+    rating: 5
+  },
+
+  {
+    id: "1015",
+    type: "skincare",
+    name: "beauty",
+    img: "https://source.unsplash.com/200x200?shampoo",
+    price: "500",
+    description: "Day cream",
+    rating: 4
+  }
+];
+
+const allProducts = () => products;
